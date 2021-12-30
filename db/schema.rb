48e9295c1102db96d05d5bf82074ca0177a66751
prasanna <prasanna.k.r@accenture.com>
2021-12-30 11:02:15 +0530

    Associations- has_one

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_051615) do
+ActiveRecord::Schema.define(version: 2021_12_30_053022) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "account_number"
+    t.integer "supplier_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["supplier_id"], name: "index_accounts_on_supplier_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -44,6 +52,13 @@ ActiveRecord::Schema.define(version: 2021_12_30_051615) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "accounts", "suppliers"
   add_foreign_key "books", "authors"
   add_foreign_key "comments", "articles"
 end
