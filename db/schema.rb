740753a979b5fa044a008026ea2cbba08a685581
prasanna <prasanna.k.r@accenture.com>
2021-12-30 10:54:00 +0530

    Associations - belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_095723) do
+ActiveRecord::Schema.define(version: 2021_12_30_051615) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 2021_12_28_095723) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -30,5 +44,6 @@ ActiveRecord::Schema.define(version: 2021_12_28_095723) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "books", "authors"
   add_foreign_key "comments", "articles"
 end
